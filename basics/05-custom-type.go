22e2bd2f2955f932390b0ab044fb4893143822d8
Amit <amit.vickey5@gmail.com>
2020-05-12 17:13:59 +0530

    Custom Data Types

---
+++ basics/05-custom-type.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// VARIABLE "myInt" is of INTEGER type
+var myInt int
+
+// CREATE A NEW CUSTOM TYPE
+// myInteger is a custom type with underlying type as INTEGER
+type myInteger int
+
+// temp is a variable of type myInteger
+var temp myInteger
+
+func main() {
+	myInt = 10
+	temp = 100
+	fmt.Printf("Built-in type variable :: value :: %d :: type :: %T\n", myInt, myInt)
+	fmt.Printf("Custom type variable :: value :: %d :: type :: %T\n", temp, temp)
+}
