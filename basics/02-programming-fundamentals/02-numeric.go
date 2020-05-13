684775108ec64a5a1a6dc59486a7a2d6940fde59
Amit <amit.vickey5@gmail.com>
2020-05-13 06:53:12 +0530

    Numeric TYPE

---
+++ basics/02-programming-fundamentals/02-numeric.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// If you use "int" as the variable type, GoLang will automatically decide whether to
+// use int32 or int64 based on the architecture of the system. Same goes with uint.
+
+func main() {
+	x := 24
+	y := 42.2345
+	fmt.Println("Value of 'x' :: ", x)
+	fmt.Printf("Type of 'x' :: %T\n", x)
+	fmt.Println("Value of 'y' :: ", y)
+	fmt.Printf("Type of 'y' :: %T\n", y)
+}
