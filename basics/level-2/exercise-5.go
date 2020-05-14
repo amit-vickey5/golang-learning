a6c4c069d750238a458fa98fa04caa4867991f74
Amit <amit.vickey5@gmail.com>
2020-05-14 09:58:09 +0530

    Level 2 :: Exercise 5

---
+++ basics/level-2/exercise-5.go
@@ -0,0 +1,14 @@
+/**
+Create a variable of type string using a raw string literal and print it.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	myRawString := `Hey there...
+Myself "Amit"...
+But people often call me "Amit Cool"`
+	fmt.Println(myRawString)
+}
