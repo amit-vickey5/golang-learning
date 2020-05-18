c874c405ad2b9c40f4055a85e9bdce62c40b882d
Amit <amit.vickey5@gmail.com>
2020-05-18 22:26:20 +0530

    Level 3 :: Exercise 5

---
+++ basics/level-3/exercise-5.go
@@ -0,0 +1,13 @@
+/**
+Print out the remainder (modulus) which is found for each number between 10 and 100 when it is divided by 4.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	for i := 10; i <= 100; i++ {
+		fmt.Printf("%d %% 4 :: %d\n", i, i % 4)
+	}
+}
