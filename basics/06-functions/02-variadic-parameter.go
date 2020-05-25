e8e1908f320b1491ab9fd2b145b44abbbee02ce9
Amit <amit.vickey5@gmail.com>
2020-05-25 12:40:20 +0530

    Variadic Parameters - Additional Info

--- basics/06-functions/02-variadic-parameter.go
+++ basics/06-functions/02-variadic-parameter.go
@@ -4,19 +4,27 @@ import (
     "fmt"
 )
 
+// Variadic means zero or more i.e. variadic function can be called with zero or more arguments
+// Variadic parameter have to be the last parameter in the function
+// If no argument is passed to the variadic function, the value passed will be "nil"
+
 func main() {
+    fmt.Println("Calling Variadic Function with some arguments :: ")
     sum := foo(2, 3, 4, 5, 6, 7, 8, 9)
-    fmt.Println("The total is ", sum)
+    fmt.Println("\t The total is ", sum)
+    fmt.Println("Calling Variadic Function with no arguments :: ")
+    sum = foo()
+    fmt.Println("\t The total is ", sum)
 }
 
 func foo(x ...int) int {
-    fmt.Printf("Type of 'x' :: %T", x)
-    fmt.Println("Values passed :: ", x)
+    fmt.Printf("\t Type of 'x' :: %T\n", x)
+    fmt.Println("\t Values passed :: ", x)
 
     sum := 0
     for index, value := range x {
         sum += value
-        fmt.Println("for item in index position, ", index, " we are now adding, ", value, " to the total which is now ", sum)
+        fmt.Println("\t for item in index position, ", index, " we are now adding, ", value, " to the total which is now ", sum)
     }
     return sum
 }
