d352880364779ea2686e51e551bac72c5dc45ad3
Amit <amit.vickey5@gmail.com>
2020-06-08 08:29:48 +0530

    Buffered channels: additional info

--- basics/10-channels/02-buffered-channel.go
+++ basics/10-channels/02-buffered-channel.go
@@ -11,6 +11,16 @@ func main() {
     c <- 42
     c <- 43
 
+    // will not work as the channel size is 2, so before putting any new value
+    // atlease one value needs to be extracted from the channel by some go-routine
+    // c <- 44
+
+    fmt.Println(<-c)
+
+    // will work now as the channel size is 2, and one value have already been extracted
+    // from the channel. so there's space for one more value now
+    c <- 44
+
     fmt.Println(<-c)
     fmt.Println(<-c)
 }
